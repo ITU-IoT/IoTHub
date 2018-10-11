e10a0764c335d9b5b52b331dcd99153ca7541196
Matthias <matt035343@gmail.com>
2018-10-11 15:10:17 +0200

    Added chromecast support and light sensor api

--- app/routes.py
+++ app/routes.py
@@ -5,6 +5,10 @@ from controllers import lightsController as lC
 from app.models import Satellite
 import json
 
+CHROMECASTS = pychromecast.get_chromecasts() #Takes time to load!
+print("Done loading CCs")
+CC_NAME = "TT"
+
 @app.route("/")
 def hello():
   s = Satellite.query.all()
@@ -20,7 +24,6 @@ def home(name):
     if not s.first():
         return render_template("home.html", name="fuck")
     return render_template("home.html", name=s.first().name)
-
 
 @app.route("/connect/<string:ip>/<int:port>/<string:name>")
 def connectSatellite(ip,port,name):
@@ -43,6 +46,67 @@ def connect():
     else:
         return render_template('home.html',name="marek")
 
+@app.route("/music/play", methods=['POST'])
+def play():
+    if not ccReady():
+        return "Chromecast not found"
+
+    mc, _ = getMediaController()
+    
+    json = request.json
+    
+    if 'url' not in json or not json['url'].endswith(".mp3"):
+        return "You must provide url to an mp3 file"
+
+    time = 0
+    if 'time' in json:
+        time = json['time']
+
+    # current time must be set to be able to seek music later
+    mc.play_media(json['url'], 'audio/mp3', current_time = time)
+    
+    return "success"
+
+@app.route("/music/update", methods=['POST'])
+def update():
+    if not ccReady():
+        return "Chromecast not found"
+
+    mc, cast = getMediaController()
+
+    json = request.json
+
+    if 'time' in json:
+        mc.seek(json['time'])
+
+    if 'volume' in json:
+        cast.set_volume(json['volume'])
+
+    return "success"
+
+@app.route("/music/pause", methods=['POST'])
+def pause():
+    if not ccReady():
+        return ("Chromecast not found")
+
+    mc, _ = getMediaController()
+
+    mc.pause()
+
+    return "success"
+
+@app.route("/sensor/light", methods=['POST'])
+def light():
+    json = request.json
+
+    if 'value' not in json:
+        return "failure, missing value"
+
+    print("Received light sensor value: ")
+    print(json['value'])
+    
+    return ""
+
 def connectPOST(request):
     if not request.json:
         return "wrong"
@@ -51,3 +115,16 @@ def connectPOST(request):
     return pC.decode(request)
 def connectGET(request):
     return request.method
+
+def ccReady():
+    if len(CHROMECASTS) == 0:
+        return False
+    casts = [cc for cc in CHROMECASTS if cc.device.friendly_name == CC_NAME]
+    if len(casts) == 0:
+        return False
+    return True
+
+def getMediaController():
+    cast = next(cc for cc in CHROMECASTS if cc.device.friendly_name == CC_NAME)
+    cast.wait()
+    return cast.media_controller, cast
