3dd8516ac5b51d1b5a7faabaab83e539244e0263
Philip <phko@itu.dk>
2018-10-26 17:23:28 +0200

    light

--- controllers/lightsController.py
+++ controllers/lightsController.py
@@ -1,10 +1,33 @@
 import json
 import requests
+from app.models import Room,Light
+from app import db
 
 address = "http://192.168.1.2/api/egZDzxX7ctoCDoXLKTxAPom6-a29XpVoQw1UvGpu/lights/"
 
 def UpdateLight(nmbr, putData):
     r = requests.put(address + str(nmbr) + "/state" , data = putData)
+
+
+def updateLights(roomIds, putData):
+  lights = db.session.query(Light).all()
+
+  for light in lights:
+    if any(roomId for roomId in roomIds if roomId == light.roomId):
+      UpdateLight(light.id, putData)
+
+
+def ChangeRoom(roomIds):
+  lights = db.session.query(Light).all()
+
+  on = "{\"on\":true}"
+  off = "{\"on\":false}"
+
+  for light in lights:
+    if any(roomId for roomId in roomIds if roomId == light.roomId):
+      UpdateLight(light.id, on)
+    else:
+      UpdateLight(light.id, off)
 
 
 '''
